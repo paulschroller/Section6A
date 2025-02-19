310102a21f325d36da1866f2a6850de5d2c610b7
paulschroller <paulschroller@ucsb.edu>
2025-02-19 22:07:51 +0000

    tasks 1 and 2

---
+++ tasks1and2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#Task 1) Average number of stars in a spherical volume of radius R: n*(4/3)*np.pi*R**3
+#if the nearest star is at distance R, that means 0 stars are in that volume
+#according to poisson distribution, the probability of 0 stars occuring in this radius would be np.exp(-n*(4/3)*np.pi*R**3)
+
+
+#Task 2) 
